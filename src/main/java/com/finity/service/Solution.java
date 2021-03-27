b6fcdaaf33c9cbe21a6d5fc086f268256ab652bc
Sergiy Velytskyy <vs@Sergiys-MacBook-Pro.local>
2021-03-27 00:20:20 -0400

    init commit

---
+++ src/main/java/com/finity/service/Solution.java
@@ -0,0 +1,75 @@
+package com.finity.service;
+
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Solution {
+	
+	private static final int MAX_PRICE = 100000;
+	private static final int MIN_PRICE = 0;
+	
+	private static final int MIN_DISCOUNT = 0;
+	private static final int MAX_DISCOUNT = 100;
+	
+	private static final int MIN_BUSKET_SIZE = 0;
+	private static final int MAX_BUSKET_SIZE = 100;
+	
+	private static final Logger LOGGER = Logger.getLogger( Solution.class.getName() );
+	
+	static {
+		LOGGER.setLevel(Level.INFO);
+	}
+	
+	public static int calculateTotalPrice(int[] prices, int discount) {
+		
+		try {
+			validateParameters(prices, discount);
+			return processBusket(prices, discount);
+			
+		}catch (CalcTotalServiceException ce){
+			
+			LOGGER.severe(ce.getMessage());
+			
+		}catch(Exception e) {
+			LOGGER.severe(e.getMessage());
+		}
+		
+		return -1;
+	}
+	
+	public static void validateParameters(int[] prices, int discount) throws  CalcTotalServiceException{
+		if(discount < MIN_DISCOUNT || discount > MAX_DISCOUNT) throw new CalcTotalServiceException("discount parameter must be between " + MIN_DISCOUNT + " and " + MAX_DISCOUNT);
+		if(prices == null || prices.length == 0) throw new CalcTotalServiceException("price busket can't be empty");
+		if(prices.length<= MIN_BUSKET_SIZE || prices.length >= MAX_BUSKET_SIZE) throw new CalcTotalServiceException("price busket size must be between " + MIN_BUSKET_SIZE + " and " + MAX_BUSKET_SIZE);
+		
+		for(int price : prices) {
+			if(price <= 0 || price >= 100000) throw new CalcTotalServiceException("price must be between " + MIN_PRICE + " and " + MAX_PRICE);
+		}
+	}
+	
+	public static int processBusket(int[] prices, int discount) {
+		
+		LOGGER.info("discount : " + discount  + " prices : " + Arrays.toString(prices));
+		Arrays.sort(prices);
+		float sum = 0;
+		for(int i = 0; i < prices.length; i++) {
+			if(i == prices.length-1) {
+				sum += prices[i]* (1- ((float)(discount)/100));
+			}
+			else {
+				sum += prices[i];
+			}
+		}
+		int result = (int)Math.ceil(sum);
+		LOGGER.info("Total sum " + result);
+		return result;
+	}
+	
+	public static void main(String args[]) {
+		int[]prices = {1000, 2000,400, 1000, 100};
+		int discount  = 25;
+		calculateTotalPrice(prices,discount);
+	}
+
+}
---
+++ src/test/java/com/finity/service/SolutionTest.java
@@ -0,0 +1,42 @@
+package com.finity.service;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SolutionTest {
+	
+	int discount;
+	int[] prices = {};
+	
+	@Test
+	public void testProcessBusket(){
+		
+		//Test case # 1 : sum 4000
+		discount = 25;
+		prices = new int[]{1000, 2000,400, 1000, 100};
+		assertTrue("First test case",Solution.calculateTotalPrice(prices, discount)==4000);
+		
+		//Test negative case #2 empty busket price
+		discount = 25;
+		prices = new int[]{};
+		assertTrue("First test case",Solution.calculateTotalPrice(prices, discount)==-1);
+		
+		//Test negative case #3 negative prices
+		discount = 25;
+		prices = new int[]{-1,100,11,12};
+		assertTrue("First test case",Solution.calculateTotalPrice(prices, discount)==-1);
+		
+		//Test negative case #4 busket size > 100
+		discount = 25;
+		prices = new int[100];
+		for(int i = 0; i < prices.length; i++)prices[i] = i;
+		assertTrue("First test case",Solution.calculateTotalPrice(prices, discount)==-1);	
+		
+		//Test case #5 only 1 item in busket price
+		discount = 25;
+		prices = new int[]{100};
+		assertTrue("First test case",Solution.calculateTotalPrice(prices, discount)==75);			
+	}
+
+}
